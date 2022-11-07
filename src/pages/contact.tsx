2c583e132cd3590dfb6e602fec207690e006f030
emih <emi.programming@gmail.com>
2022-11-07 22:23:08 +0900

    ✨ add a contact form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,16 @@
 import styles from "src/styles/Contact.module.css";
-("");
+
 const Contact = () => {
   return (
     <div className={styles.contact}>
       <div className={styles.container}>
-        <h2>Contactページ制作中</h2>
+        <div>
+          <iframe
+            className={styles.iframe}
+            height="734px"
+            src="https://www.noway-form.com/f/6e251ea0-c037-4815-b757-17640222d7d2/embed"
+          />
+        </div>
       </div>
     </div>
   );
