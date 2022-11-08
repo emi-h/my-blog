dec85f9abf80a086c255361004992ddc1ef5d25d
emih <emi.programming@gmail.com>
2022-11-08 11:44:28 +0900

    ✨ add 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import styles from "src/styles/404.module.css";
+
+const Custom404 = () => {
+  return (
+    <main className={styles.main}>
+      <p>ページがありません。</p>
+      <br />
+      <br />
+      <p>
+        <Link href="/">トップページへ戻る</Link>
+      </p>
+    </main>
+  );
+};
+export default Custom404;
