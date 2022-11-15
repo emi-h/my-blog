3451792bdc9c3b6647104749db6fff25f0af0f7a
emih <emi.programming@gmail.com>
2022-11-15 21:38:34 +0900

    ✨ add GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,21 @@
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
+import GoogleTagManager, {
+  GoogleTagManagerId,
+} from "src/components/GoogleTagManager";
 import { Layout } from "src/components/Layout/Layout";
+import { googleTagManagerId } from "src/types/gtm";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <GoogleTagManager
+        googleTagManagerId={googleTagManagerId as GoogleTagManagerId}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
