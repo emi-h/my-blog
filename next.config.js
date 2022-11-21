67af4a39e2bba7a8e5f3893197f0e9b1e5639112
emih <emi.programming@gmail.com>
2022-11-21 22:39:57 +0900

    🐛

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.microcms-assets.io", "scontent.cdninstagram.com"],
+    domains: [
+      "images.microcms-assets.io",
+      "scontent.cdninstagram.com",
+      "%3A%2F%2Fscontent.cdninstagram.com",
+    ],
   },
   reactStrictMode: true,
   swcMinify: true,
